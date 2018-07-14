09fad5e69c4b3ad440eb2979390200b7ccf63d3a
NathanYukai <zlcghoul@hotmail.com>
2018-07-14 22:58:47 +0100

    use space as confirmation ey

--- src/Input.hs
+++ src/Input.hs
@@ -1,8 +1,8 @@
 module Input where
 
-import Data.Map as Map
 import Text.Read
 import Data.Char
+import qualified Text.Read.Lex as L
 
 data Input = A
            | B
@@ -18,8 +18,41 @@ data Input = A
            | M
            | J
            | K
+           | SPC
            | Input Int
-           deriving (Eq, Show, Read)
+           deriving (Eq, Show)
+
+-- simplyfy this?
+instance Read Input where
+  readPrec = parens (
+    do L.Ident s <- lexP
+       case s of
+         "A" -> return A
+         "B" -> return B
+         "C" -> return C
+         "D" -> return D
+         "E" -> return E
+         "F" -> return F
+         "G" -> return G
+         "H" -> return H
+         "W" -> return W
+         "S" -> return S
+         "Q" -> return Q
+         "M" -> return M
+         "J" -> return J
+         "K" -> return K
+         " " -> return SPC
+         "1" -> return $ Input 1
+         "2" -> return $ Input 2
+         "3" -> return $ Input 3
+         "4" -> return $ Input 4
+         "5" -> return $ Input 5
+         "6" -> return $ Input 6
+         "7" -> return $ Input 7
+         "8" -> return $ Input 8
+         "9" -> return $ Input 9
+         _ -> pfail
+    )
 
 getInput :: IO (Input)
 getInput = do
